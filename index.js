6c0ee55173f70a9d8d98934d94cc56ac3a299cce
Mathias Buus <mathiasbuus@gmail.com>
2020-07-21 17:24:50 +0200

    dedup rules

--- index.js
+++ index.js
@@ -1,11 +1,76 @@
-module.exports = minisheet
+const enabled = new Set()
+let main
 
-function minisheet (strs) {
-  const s = strs[0]
-  const cls = 'ms' + Math.random().toString(16).slice(2)
-  const e = document.createElement('style')
-  e.setAttribute('type', 'text/css')
-  e.textContent += s.replace(/:host/g, '.' + cls)
-  document.head.appendChild(e)
-  return cls
+module.exports = css
+
+function css (strs, ...args) {
+  const host = '_ms' + Math.random().toString(16).slice(2)
+  let s = strs[0]
+  let i = 1
+  for (const arg of args) s += arg + strs[i++]
+  for (const rule of rules(s.replace(/:host/g, '.' + host))) {
+    if (enabled.has(rule)) continue
+    enabled.add(rule)
+
+    if (!main) {
+      main = document.createElement('style')
+      main.setAttribute('type', 'text/css')
+      document.head.appendChild(main)
+    }
+
+    main.sheet.insertRule(rule, main.sheet.cssRules.length)
+  }
+
+  return host
+}
+
+function rules (s) {
+  const all = []
+  let rule = ''
+  let string = ''
+  let escaped = false
+  let ctrl = true
+
+  for (let i = 0; i < s.length; i++) {
+    const ch = s[i]
+
+    const wasCtrl = ctrl
+    ctrl = false
+
+    if (escaped) {
+      rule += '\\' + ch
+      escaped = false
+      continue
+    }
+
+    if (string) {
+      rule += ch
+      if (ch === string) string = ''
+      continue
+    }
+
+    if (ch === '{' || ch === ';' || ch === ':') {
+      ctrl = true
+    }
+
+    if (ch === '"' || ch === "'") {
+      string = ch
+    }
+
+    if (ch === '}') {
+      all.push(rule + ch)
+      rule = ''
+      ctrl = true
+      continue
+    }
+
+    if (ch === ' ' || ch === '\n' || ch === '\t') {
+      ctrl = true
+      if (wasCtrl) continue
+    }
+
+    rule += ch
+  }
+
+  return all
 }
